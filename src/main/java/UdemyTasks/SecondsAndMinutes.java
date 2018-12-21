a565f70d5cfc237d4454fe2b9432e78bdd1a9555
kmisko <kmisko11@gmail.com>
2018-12-21 22:43:48 +0100

    test

---
+++ src/main/java/UdemyTasks/SecondsAndMinutes.java
@@ -0,0 +1,26 @@
+package UdemyTasks;
+
+public class SecondsAndMinutes {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static String getDurationString(long minutes, long second){
+    if (minutes < 0 || second < 0 || second > 59){
+      return "Invalid value";
+    }
+    long hours = minutes / 60;
+    long remainingMinutes = minutes % 60;
+    return hours + "h " + remainingMinutes + "min " + second + "s";
+  }
+  public static String getDurationString(long second){
+    if (second < 0){
+      return "Invalid value";
+    }
+    long  minutes = second / 60;
+    long remainingSeconds = second % 60;
+
+    return getDurationString(minutes, remainingSeconds);
+  }
+}
